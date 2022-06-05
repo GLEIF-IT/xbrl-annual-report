1593b0000ce3ede12d82e17ce8473a93700578b9
pfeairheller <pfeairheller@gmail.com>
2022-06-05 15:03:18 -0700

    Moved script to new location for generating signed report.

---
+++ scripts/generate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 ~/git_root/arelle/arelleCmdLine.py --plugins=$HOME/git_root/ixbrl-viewer/iXBRLViewerPlugin/__init__.py -f ~/Downloads/gleif-annual-report-2020/reports/gleif-2020-12-31.xhtml --save-viewer ixbrl-report-viewer.html --viewer-url "./ixbrlviewer.js" --packages ./example/gleif.zip
